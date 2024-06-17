e6ad18154a1eabffcb30ac9ac413c60b85d401f1
Khamisi Kibet <82152373+KhamisiKibet@users.noreply.github.com>
2024-06-17 08:11:51 -0700

    Update textAll.py

--- PySideExtn/examples/rpb/textAll.py
+++ PySideExtn/examples/rpb/textAll.py
@@ -1,44 +1,44 @@
 import sys
 from PySide2 import QtCore, QtWidgets, QtGui
 #IMPORTING THE MODULE
-from PySideExtn.RoundProgressBar import roundProgressBar
+from PySideExtn.RoundProgressBar import RoundProgressBar
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
         
         #CLASS INSTANCE
-        self.rpb = roundProgressBar()
-        self.rpb2 = roundProgressBar()
-        self.rpb3 = roundProgressBar()
+        self.rpb = RoundProgressBar()
+        self.rpb2 = RoundProgressBar()
+        self.rpb3 = RoundProgressBar()
 
         #SETTING THE RANGE : MIN-0 & MAX:360
-        self.rpb.rpb_setRange(0, 360) 
-        self.rpb2.rpb_setRange(0, 360)
-        self.rpb3.rpb_setRange(0, 360)
+        self.rpb.setRange(0, 360) 
+        self.rpb2.setRange(0, 360)
+        self.rpb3.setRange(0, 360)
         
         #CHANGING THE STYLE
-        self.rpb.rpb_setBarStyle('Pizza')
-        self.rpb2.rpb_setBarStyle('Hybrid2')
+        self.rpb.setBarStyle('Pizza')
+        self.rpb2.setBarStyle('Hybrid2')
 
         #CHANGING THE TEXT TYPE : VALUE OR PERCENTAGE
-        self.rpb.rpb_setTextFormat('Value')
-        self.rpb2.rpb_setTextFormat('Percentage')
+        self.rpb.setTextFormat('Value')
+        self.rpb2.setTextFormat('Percentage')
 
         #CHANGING THE TEXT SIZE
-        self.rpb.rpb_setTextRatio(3)
-        self.rpb2.rpb_setTextRatio(9)
+        self.rpb.setTextRatio(3)
+        self.rpb2.setTextRatio(9)
 
         #CHANGING THE FONT
-        self.rpb.rpb_setTextFont('Arial')
-        self.rpb2.rpb_setTextFont('Times New Roman')
+        self.rpb.setTextFont('Arial')
+        self.rpb2.setTextFont('Times New Roman')
 
         #TEXT HIDDEN
-        self.rpb3.rpb_enableText(False)
+        self.rpb3.enableText(False)
         
         #SETTING THE VALUE
-        self.rpb.rpb_setValue(156)
-        self.rpb2.rpb_setValue(156)
+        self.rpb.setValue(156)
+        self.rpb2.setValue(156)
 
         self.layout = QtWidgets.QHBoxLayout()
         self.layout.addWidget(self.rpb)
