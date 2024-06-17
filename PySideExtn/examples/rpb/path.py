4e1de52f71f4ef4c988fb0fe7e878f5d917f66ae
Khamisi Kibet <82152373+KhamisiKibet@users.noreply.github.com>
2024-06-17 08:11:30 -0700

    Update path.py

--- PySideExtn/examples/rpb/path.py
+++ PySideExtn/examples/rpb/path.py
@@ -1,34 +1,34 @@
 import sys
 from PySide2 import QtCore, QtWidgets, QtGui
 #IMPORTING THE MODULE
-from PySideExtn.RoundProgressBar import roundProgressBar
+from PySideExtn.RoundProgressBar import RoundProgressBar
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
         
         #CLASS INSTANCE
-        self.rpb = roundProgressBar()
-        self.rpb2 = roundProgressBar()
+        self.rpb = RoundProgressBar()
+        self.rpb2 = RoundProgressBar()
 
         #CHANGING THE PROGRESSABR STYLE
-        self.rpb.rpb_setBarStyle('Hybrid1')
+        self.rpb.setBarStyle('Hybrid1')
 
         #CHANGING THE LINE COLOR AND WIDTH
-        self.rpb.rpb_setLineWidth(3)
-        self.rpb2.rpb_setLineWidth(8)
+        self.rpb.setLineWidth(3)
+        self.rpb2.setLineWidth(8)
 
         #PATH WIDTH
-        self.rpb.rpb_setPathWidth(15)
-        self.rpb2.rpb_setPathWidth(2)
+        self.rpb.setPathWidth(15)
+        self.rpb2.setPathWidth(2)
 
         #CHANGING THE PATH COLOR
-        self.rpb.rpb_setPathColor((125, 255, 255))
-        self.rpb2.rpb_setPathColor((0, 0, 0))
+        self.rpb.setPathColor((125, 255, 255))
+        self.rpb2.setPathColor((0, 0, 0))
         
         #SETTING THE VALUE
-        self.rpb.rpb_setValue(85)
-        self.rpb2.rpb_setValue(85)
+        self.rpb.setValue(85)
+        self.rpb2.setValue(85)
 
 
         self.layout = QtWidgets.QHBoxLayout()
