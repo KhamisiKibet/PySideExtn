e15d2c7eface3d0aee160e0beed34d2a13791656
Khamisi Kibet <82152373+KhamisiKibet@users.noreply.github.com>
2024-06-17 08:12:45 -0700

    Update max,min,range.py

--- PySideExtn/examples/spb/max,min,range.py
+++ PySideExtn/examples/spb/max,min,range.py
@@ -1,7 +1,7 @@
 import sys
 from PySide2 import QtCore, QtWidgets, QtGui
 
-from PySideExtn.SpiralProgressBar import spiralProgressBar #IMPORT THE EXTENSION LIBRARY
+from PySideExtn.SpiralProgressBar import SpiralProgressBar #IMPORT THE EXTENSION LIBRARY
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
@@ -9,18 +9,18 @@ class MyWidget(QtWidgets.QWidget):
 
         self.hello = 'Spiral Progress Bar'
         
-        self.spbMinMax = spiralProgressBar()    #SPIRAL PROGRESSBAR OBJECT
-        self.spbRange = spiralProgressBar()     #SPIRAL PROGRESS BAR OBJECT
+        self.spbMinMax = SpiralProgressBar()    #SPIRAL PROGRESSBAR OBJECT
+        self.spbRange = SpiralProgressBar()     #SPIRAL PROGRESS BAR OBJECT
 
         #SPIRAL PROGRESS BAR spbMinMax : GREEN COLOR
-        self.spbMinMax.spb_setMinimum((0, 0, 0))    #SETTING THE MINIMUM VALUE
-        self.spbMinMax.spb_setMaximum((360, 360, 360))   #SETTING THE MAXIMUM VALUE 
-        self.spbMinMax.spb_lineColor(((0, 255, 0), (0, 255, 0), (0, 255, 0)))  #GREEN COLOR
-        self.spbMinMax.spb_setValue((300, 350, 289))    #SET THE CURRENT VALUE
+        self.spbMinMax.setMinimum((0, 0, 0))    #SETTING THE MINIMUM VALUE
+        self.spbMinMax.setMaximum((360, 360, 360))   #SETTING THE MAXIMUM VALUE 
+        self.spbMinMax.lineColor(((0, 255, 0), (0, 255, 0), (0, 255, 0)))  #GREEN COLOR
+        self.spbMinMax.setValue((300, 350, 289))    #SET THE CURRENT VALUE
         
         #SPIRAL PROGRESS BAR spbRange :BLUE COLOR
-        self.spbRange.spb_setRange((0, 0, 0), (360, 360, 360))  #SETTING THE RANGE
-        self.spbRange.spb_setValue((300, 350, 289)) #SETTING CURRENT VALUE
+        self.spbRange.setRange((0, 0, 0), (360, 360, 360))  #SETTING THE RANGE
+        self.spbRange.setValue((300, 350, 289)) #SETTING CURRENT VALUE
         
         self.layout = QtWidgets.QHBoxLayout()
         self.layout.addWidget(self.spbMinMax)
