3dc9a4cda46101218cd3148174c95d0a02901441
Khamisi Kibet <82152373+KhamisiKibet@users.noreply.github.com>
2024-06-17 08:12:26 -0700

    Update linecolor,style,e.t.c.py

--- PySideExtn/examples/spb/linecolor,style,e.t.c.py
+++ PySideExtn/examples/spb/linecolor,style,e.t.c.py
@@ -1,7 +1,7 @@
 import sys
 from PySide2 import QtCore, QtWidgets, QtGui
 
-from PySideExtn.SpiralProgressBar import spiralProgressBar #IMPORT THE EXTENSION LIBRARY
+from PySideExtn.SpiralProgressBar import SpiralProgressBar #IMPORT THE EXTENSION LIBRARY
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
@@ -9,23 +9,23 @@ class MyWidget(QtWidgets.QWidget):
 
         self.hello = 'Spiral Progress Bar'
         
-        self.spbN = spiralProgressBar()    #SPIRAL PROGRESSBAR OBJECT
-        self.spbN.spb_setNoProgressBar(4)
+        self.spbN = SpiralProgressBar()    #SPIRAL PROGRESSBAR OBJECT
+        self.spbN.setNoProgressBar(4)
         
         #LINE WIDTH: 15px
-        self.spbN.spb_lineWidth(15)
-        self.spbN.spb_setGap(17)
+        self.spbN.lineWidth(15)
+        self.spbN.setGap(17)
 
         #LINE COLOR
-        self.spbN.spb_lineColor(((0, 125, 125), (125, 0, 125), (125, 255, 0), (125, 125, 125)))
+        self.spbN.lineColor(((0, 125, 125), (125, 0, 125), (125, 255, 0), (125, 125, 125)))
 
         #LINE STYLE
-        self.spbN.spb_lineStyle(('SolidLine', 'DotLine', 'DashLine', 'SolidLine'))
+        self.spbN.lineStyle(('SolidLine', 'DotLine', 'DashLine', 'SolidLine'))
 
         #LINE CAP
-        self.spbN.spb_lineCap(('SquareCap', 'RoundCap', 'RoundCap', 'SquareCap'))
+        self.spbN.lineCap(('SquareCap', 'RoundCap', 'RoundCap', 'SquareCap'))
 
-        self.spbN.spb_setValue((55, 55, 55, 55))
+        self.spbN.setValue((55, 55, 55, 55))
 
         self.layout = QtWidgets.QHBoxLayout()
         self.layout.addWidget(self.spbN)
